0ee86f6d19f66a3eb655dba476b345fb65e58e17
NICK D <nickdouroudakis@gmail.com>
2024-11-24 21:43:29 +0200

    Add files via upload

---
+++ Hello_oss1_ex.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.hello_oss1.hello_oss1_ex;
+
+/**
+ *
+ * @author nickd
+ */
+public class Hello_oss1_ex {
+
+    public static void main(String[] args) {
+        
+        
+        String s ="hello World\n";
+        System.out.println("s");
+        System.out.println(s.toUpperCase() );
+        System.err.println("length of the String is:"+ s.length());
+        System.out.println("Hello World!");
+    }
+}
